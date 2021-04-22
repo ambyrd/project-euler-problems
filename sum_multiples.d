fe5c2f02ca56f3b1b657da3e3f602aa7865c0cdf
Andrew Byrd <andrewbyrd256@gmail.com>
2021-04-21 21:08:13 -0500

    Rework sum function and add some basic tests

--- sum_multiples.d
+++ sum_multiples.d
@@ -8,14 +8,27 @@ Find the sum of all the multiples of 3 or 5 below 1000.
 
 import std.stdio;
 
-void main() {
-    int max = 1000;
+
+int sum_multiples(int max) {
     int sum = 0;
     for(int n = 0; n < max; n++) {
         if(n % 3 == 0 || n % 5 == 0) {
             sum += n;
         }
     }
+    return sum;
+}
 
+unittest {
+    // Example from problem description
+    assert(sum_multiples(10) == 23);
+    // Base cases (less than 3)--should return 0
+    assert(sum_multiples(2) == 0);
+    assert(sum_multiples(1) == 0);
+}
+
+void main() {
+    int max = 1000;
+    int sum = sum_multiples(max);
     writefln("Sum of multiples of 3 or 5 below %s: %s", max, sum);
 }
