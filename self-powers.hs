cd87d2ff064dfd0cc2b6588745775f3e3398fd3c
Andrew Byrd <andrewbyrd256@gmail.com>
2021-05-28 20:30:39 -0500

    Change series_sum to a function

--- self-powers.hs
+++ self-powers.hs
@@ -7,9 +7,9 @@
 selfpow :: Integral b => b -> b
 selfpow n = n^n
 
-series_sum :: Integer
-series_sum = sum (map selfpow [1..1000])
+series_sum :: [Integer] -> Integer
+series_sum = sum . map selfpow
 
 main :: IO ()
 main = do putStrLn "Last ten digits of the series 1^1 + 2^2 + 3^3 + ... + 1000^1000:"
-          putStrLn (show (series_sum `mod` 10000000000))  --using modulo to extract the last 10 digits
+          putStrLn (show (series_sum [1..1000] `mod` 10000000000))  --using modulo to extract the last 10 digits
